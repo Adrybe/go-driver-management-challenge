bb2cf46765b6d343123f83bd7d5ccd847550cc49
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 01:36:53 -0300

    set port

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,15 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"os"
+)
 
 func main() {
+	port := os.Getenv("PORT")
+	port = ":" + port
+
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -10,4 +17,5 @@ func main() {
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	http.ListenAndServe(port, r)
 }
