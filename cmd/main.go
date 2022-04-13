13e7e6a3d03ba18e7c38cb8d329e654ebf623d00
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 02:20:11 -0300

    change port setttings

--- cmd/main.go
+++ cmd/main.go
@@ -2,21 +2,19 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"log"
 	"net/http"
-	"os"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-	port = ":" + port
+	//port := os.Getenv("PORT")
+	//port = ":" + port
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "message : pong")
 	})
 	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	log.Println("Server started on port " + port)
-	r.Run(":3000")
+	//log.Println("Server started on port " + port)
+	r.Run()
 	//http.ListenAndServe(port, r)
 }
