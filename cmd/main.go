016788f911b400b08ba866227f077de78be72d9f
Adrybe <adrianenriquebenitez@gmail.com>
2022-04-13 02:51:16 -0300

    change http run

--- cmd/main.go
+++ cmd/main.go
@@ -8,16 +8,12 @@ import (
 
 func main() {
 	port := os.Getenv("PORT")
-	if len(port) == 0 {
-		port = "8080"
-	}
-	//port = ":" + port
+	port = ":" + port
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.String(http.StatusOK, "message : pong")
 	})
-	//r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
-	//log.Println("Server started on port " + port)
-	http.ListenAndServe(":"+port, r)
+
+	r.Run()
 }
